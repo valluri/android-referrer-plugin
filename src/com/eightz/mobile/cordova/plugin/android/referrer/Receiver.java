5327ad214cd9198c103655c4b3b34635d66a7093
rashmi-tondare <rashmi-tondare@users.noreply.github.com>
2016-10-20 22:49:28 +0530

    modified receiver

--- src/com/eightz/mobile/cordova/plugin/android/referrer/Receiver.java
+++ src/com/eightz/mobile/cordova/plugin/android/referrer/Receiver.java
@@ -10,6 +10,8 @@ import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 
 import android.util.Log;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 public class Receiver extends BroadcastReceiver {
 
@@ -19,14 +21,29 @@ public void onReceive(Context context, Intent intent) {
     Bundle extras = intent.getExtras();
     if (extras != null) {
     	String referrerString = extras.getString("referrer");
-        if (referrerString != null) {
-    	    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-
-    	    Editor edit = sharedPreferences.edit();
-    	    edit.putString("referrer", referrerString);
-    	    edit.commit();
+        if (referrerString != null && !referrerString.contains("organic")) {
+            trackReferrerAttributes(context, referrerString);
         }
     }
 }
+
+private void trackReferrerAttributes(Context context, String rawReferrer) {
+        String referrer;
+        try {
+            referrer = URLDecoder.decode(rawReferrer, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            Log.e("Receiver", "Error in decoding referrer code into UTF-8 form");
+            Log.e("Receiver", e.getMessage(), e);
+            return;
+        }
+
+        String json = "{\"referrer\":\"" + referrer + "\"}";
+        Log.d("Receiver", json);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("NativeStorage", Context.MODE_PRIVATE);
+    	Editor edit = sharedPreferences.edit();
+    	edit.putString("referrer", json);
+    	edit.commit();
+    }
  
 } // end of class
+
